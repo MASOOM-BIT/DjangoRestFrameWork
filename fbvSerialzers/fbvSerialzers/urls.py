d2167a26021fdbfb18bf3a66ccda1268eb8bd87c
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-05-27 02:19:57 +0530

    Add URL patterns for student list and detail views in fbvSerialzers

--- fbvSerialzers/fbvSerialzers/urls.py
+++ fbvSerialzers/fbvSerialzers/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from fbvApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('students/', views.student_list, name='student_list'),
+    path('students/<int:pk>', views.student_detail, name='student_detail'),
 ]
