f3fe41ec3d971f1879316578c0e49375abf15230
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-05-29 00:51:38 +0530

    Refactor views to use mixins for StudentList and StudentDetail classes

--- cbvSerializers/cbvApp/views.py
+++ cbvSerializers/cbvApp/views.py
@@ -6,7 +6,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from django.http import Http404
 # Create your views here.
-
+'''
 class StudentList(APIView):
     def get(self, request):
         students = Student.objects.all()
@@ -44,3 +44,34 @@ class StudentDetail(APIView):
         student = self.get_object(pk)
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+        '''
+ #Mixins classes
+    # ListModelMixin -> list() -> get()
+    # CreateModelMixin -> create() -> post()
+    # RetrieveModelMixin -> retrieve() -> get()
+    # UpdateModelMixin -> update() -> put()
+    # DestroyModelMixin -> destroy() -> delete()
+
+from rest_framework import mixins, generics
+class StudentList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+class StudentDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, pk):
+        return self.retrieve(request, pk=pk)
+
+    def put(self, request, pk):
+        return self.update(request, pk=pk)
+
+    def delete(self, request, pk):
+        return self.destroy(request, pk=pk)
