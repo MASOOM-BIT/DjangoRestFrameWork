6d7ebf361a86498ec073fe1cb558402398a873cd
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-06-01 00:59:24 +0530

    Add pagination settings to REST framework in settings.py

--- cbvSerializers/cbvSerializers/settings.py
+++ cbvSerializers/cbvSerializers/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'rest_framework',
     'cbvApp',  # Your app name
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
