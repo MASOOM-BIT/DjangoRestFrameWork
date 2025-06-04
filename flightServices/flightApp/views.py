eb4e02034b9285b9eb781952fef951410dbaf858
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-06-05 02:22:16 +0530

    Add save_reservation view to handle flight reservations

--- flightServices/flightApp/views.py
+++ flightServices/flightApp/views.py
@@ -4,6 +4,7 @@ from flightApp.serializers import FlightSerializer, PassengerSerializer, Reserva
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 # Create your views here.
 @api_view(['POST'])
 def find_flight(request):
@@ -14,6 +15,27 @@ def find_flight(request):
     )
     serializer = FlightSerializer(flights, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def save_reservation(request):
+    flight = Flight.objects.get(id=request.data['flightId'])
+    passanger = Passanger()
+    passanger.firstName = request.data['firstName']
+    passanger.lastName = request.data['lastName']
+    passanger.middleName = request.data['middleName']
+    passanger.email = request.data['email']
+    passanger.phone = request.data['phone']
+    passanger.save()
+
+    reservation = Reservation()
+    reservation.flight = flight
+    reservation.passanger = passanger
+    reservation.save()
+    return Response(status=status.HTTP_201_CREATED)
+
+
+
+
 
 
 class FlightViewSet(viewsets.ModelViewSet):
