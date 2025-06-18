39d9ebbfd0535a97feff83d0debfd5a22ff3a7d4
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-06-19 01:24:43 +0530

    feat: add initial DisplayFlights, FindFlights, and PassengerDetails components

---
+++ flightReservation/src/app/components/display-flights/display-flights.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-flights',
+  imports: [],
+  templateUrl: './display-flights.html',
+  styleUrl: './display-flights.css'
+})
+export class DisplayFlights {
+
+}
---
+++ flightReservation/src/app/components/find-flights/find-flights.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-find-flights',
+  imports: [],
+  templateUrl: './find-flights.html',
+  styleUrl: './find-flights.css'
+})
+export class FindFlights {
+
+}
