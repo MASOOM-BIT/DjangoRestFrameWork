f44988f122820b251172d6318e2a8bba57ee882b
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-06-21 01:15:57 +0530

    feat: implement Reservation service with flight retrieval functionality

--- flightReservation/src/app/services/reservation.ts
+++ flightReservation/src/app/services/reservation.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Login } from './login';
 @Injectable({
   providedIn: 'root'
 })
 export class Reservation {
+  flightsUrl : string = 'http://127.0.0.1:8000/flightServices/findFlights/';
+  singleFlightUrl : string = 'http://127.0.0.1:8000/flightServices/flights/';
+  saveReservationUrl : string = 'http://127.0.0.1:8000/flightServices/saveReservation/';
 
-  constructor() { }
+
+  constructor(private http:HttpClient,private loginService:Login) { }
+public getFlights(criteria):any{
+  return this.http.post(this.flightsUrl,criteria,this.loginService.httpOptions);
+}
 }
